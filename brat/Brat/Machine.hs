c6c55ae7dda8eff735ceea3c2f9608d4fcf3400b
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-12-09 16:30:44 +0000

    WIP Eval, Box with captureSets and ReturnTo

--- brat/Brat/Machine.hs
+++ brat/Brat/Machine.hs
@@ -1,9 +1,10 @@
 module Brat.Machine (runInterpreter) where
 
+import Brat.Checker.Monad (CaptureSets)
 import Brat.Compiler (compileToGraph)
 import Brat.Naming (Name, Namespace)
 import Brat.Graph (Graph, NodeType (..), Node (BratNode, KernelNode), wiresTo, MatchSequence (..), PrimTest (..), TestMatchData (..))
-import Brat.QualName (plain)
+import Brat.QualName (QualName, plain)
 import Brat.Syntax.Simple (SimpleTerm(..))
 import Brat.Syntax.Port (OutPort(..))
 import Brat.Syntax.Common
@@ -11,15 +12,19 @@ import Brat.Syntax.Value
 
 import Hasochism
 
+import Data.Maybe (fromMaybe)
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Bwd
+
+type GraphInfo = (Graph, CaptureSets)
 
 runInterpreter :: [FilePath] -> String -> String -> IO ()
 runInterpreter libDirs file runFunc = do
-    (_, (venv, _, _, _, outerGraph, _)) <- compileToGraph libDirs file
+    (_, (venv, _, _, _, outerGraph, capSets)) <- compileToGraph libDirs file
     print (show outerGraph)
     let outPorts = [op | (NamedPort op _, _ty) <- venv M.! (plain runFunc)]
-    let outTask = evalPorts outerGraph (B0 :< BratValues M.empty) B0 outPorts
+    let outTask = evalPorts (outerGraph,capSets) (B0 :< BratValues M.empty) B0 outPorts
     -- we hope outTask is a Finished. Or a Suspend.
     print outTask
     pure ()
@@ -31,12 +36,15 @@ data Frame where
     EvalPorts :: Bwd Value -> [OutPort] -> Frame
     PortOfNode :: OutPort -> Frame
     HandleNodeOutputs :: OutPort -> Frame
+    -- have arguments to function, waiting for the function:
+    CallWith :: [Value] -> Frame
+    ReturnTo :: Bwd Frame -> Frame
   deriving Show
 
 data Task where
     EvalPort :: OutPort -> Task
     Suspend :: [Frame] -> Task -> Task
-    EvalNode :: Node -> [Value] -> Task
+    EvalNode :: Name -> [Value] -> Task
     Use :: Value -> Task -- searches for EvalPorts
     Finished :: [Value] -> Task -- searches for HandleNodeOutputs, or final result
   deriving Show
@@ -46,46 +54,63 @@ lookupOutport B0 _ = Nothing
 lookupOutport (_ :< BratValues env) p | Just v <- M.lookup p env = Just v
 lookupOutport (fz :< _) p = lookupOutport fz p
 
-evalPorts :: Graph -> Bwd Frame -> Bwd Value -> [OutPort] -> Task
+evalPorts :: GraphInfo -> Bwd Frame -> Bwd Value -> [OutPort] -> Task
 -- EvalPorts is "missing" one input (between valz and ports), i.e. the one that's the current Task
 -- (whereas evalPorts has them all)
 evalPorts g fz valz (p:ps) = run g (fz :< EvalPorts valz ps) (EvalPort p)
 evalPorts g fz valz [] = run g fz (Finished (valz <>> []))
 
+evalNodeInputs :: GraphInfo -> Bwd Frame -> Name -> Task
+evalNodeInputs gi@(g,_) fz name =
+    -- might be good to check M.keys == [0,1,....] here
+    let srcs = M.elems (M.fromList [(tgtPort, src) | (src, _, In _ tgtPort) <- wiresTo name g])
+    in evalPorts gi fz B0 srcs
+
 updateCache (fz :< BratValues env) port_vals = fz :< (BratValues $ foldr (uncurry M.insert) env port_vals)
 updateCache (fz :< f) pvs = (updateCache fz pvs) :< f
 -- updateCache B0 pvs = B0 :< (M.fromList pvs)
 
-run :: Graph -> Bwd Frame -> Task -> Task
+run :: GraphInfo -> Bwd Frame -> Task -> Task
 -- Tasks that push new frames onto the stack to do things
-run g@(nodes, wires) fz (EvalPort p@(Ex name offset)) = case lookupOutport fz p of
-    Just v -> run g fz (Use v)
-    Nothing -> 
-        -- might be good to check M.keys == [0,1,....] here
-        let srcs = M.elems (M.fromList [(tgtPort, src) | (src, _, In _ tgtPort) <- wiresTo name g])
-        in evalPorts g (fz :< PortOfNode p) B0 srcs
-run g fz (EvalNode (BratNode (Const st) _ _) []) = run g fz (Finished [evalSimpleTerm st])
-run g fz (EvalNode (BratNode (ArithNode op) _ _) ins) = run g fz (Finished [evalArith op ins])
-run g fz (EvalNode (BratNode Id _ _) ins) = run g fz (Finished ins)
-
+run gi@(g@(nodes, wires), _) fz (EvalPort p@(Ex name offset)) = case lookupOutport fz p of
+    Just v -> run gi fz (Use v)
+    Nothing -> evalNodeInputs gi (fz :< PortOfNode p) name
+run g@((nodes, _), cs) fz t@(EvalNode n ins) = case nodes M.! n of
+    (BratNode (Const st) _ _) -> run g fz (Finished [evalSimpleTerm st])
+    (BratNode (ArithNode op) _ _) -> run g fz (Finished [evalArith op ins])
+    (BratNode Id _ _) -> run g fz (Finished ins)
+    (BratNode (Eval func) _ _) -> run g (fz :< CallWith ins) (EvalPort func)
+    (BratNode (Box src tgt) _ _) ->
+        let captureSet = fromMaybe M.empty (M.lookup n cs)
+            capturedSrcs = S.fromList [src | (NamedPort src _name, _ty) <- concat (M.elems captureSet)]
+        in run g fz (Finished [ThunkV $ BratClosure (captureEnv fz capturedSrcs) src tgt])
+    _ -> run g fz (Suspend [] t)
 -- Tasks that unwind the stack looking for what to do with the result
-run g (fz :< f) (Suspend fs t) = run g fz (Suspend (f:fs) t)
+----Suspend
+run gi (fz :< f) (Suspend fs t) = run gi fz (Suspend (f:fs) t)
 run _ B0 t@(Suspend _ _) = t
-run g (fz :< EvalPorts valz rem) (Use v) = evalPorts g fz (valz :< v) rem
-run g@(nodes, _) (fz :< PortOfNode req@(Ex name offset)) (Finished inputs) =
-    run g (fz :< HandleNodeOutputs req) (EvalNode (nodes M.! name) inputs)
+---- Use (single value)
+run gi (fz :< EvalPorts valz rem) (Use v) = evalPorts gi fz (valz :< v) rem
+run gi (fz :< CallWith inputs) (Use (ThunkV (BratClosure env src tgt))) =
+    let env_with_args = foldr (uncurry M.insert) env [(Ex src off, val) | (off, val) <- zip [0..] inputs]
+    in evalNodeInputs gi (B0 :< ReturnTo fz :< (BratValues env_with_args)) tgt
 
+---- Finished (list of values)
+run g (fz :< PortOfNode req@(Ex name offset)) (Finished inputs) =
+    run g (fz :< HandleNodeOutputs req) (EvalNode name inputs)
 run g (fz :< HandleNodeOutputs req@(Ex name offset)) (Finished outputs) =
     run g (updateCache fz [(Ex name i, val) | (i, val) <- zip [0..] outputs]) (Use (outputs !! offset))
+run g (B0 :< ReturnTo fz) (Finished vals) = run g fz (Finished vals)
 
 run g (fz :< BratValues _) t = run g fz t
+run g B0 t = t
 run g fz t = run g fz (Suspend [] t)
 
 
-buildEnv :: Bwd Frame -> EvalEnv
-buildEnv B0 = M.empty
-buildEnv (fz :< BratValues env) = M.union (buildEnv fz) env
-buildEnv (fz :< _) = buildEnv fz
+captureEnv :: Bwd Frame -> S.Set OutPort -> EvalEnv
+captureEnv B0 _ = M.empty
+captureEnv (fz :< BratValues env) keys = M.union (captureEnv fz keys) (M.filterWithKey (\k _ -> S.member k keys) env)
+captureEnv (fz :< _) keys = captureEnv fz keys
 
 evalSimpleTerm :: SimpleTerm -> Value
 evalSimpleTerm (Num x) = IntV x
