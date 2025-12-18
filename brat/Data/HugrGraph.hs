57f211064ecbf00a80f104a15c249cbbf99807f5
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-12-18 11:08:26 +0000

    HugrGraph: separate out root

--- brat/Data/HugrGraph.hs
+++ brat/Data/HugrGraph.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.HugrGraph(NodeId, PortId(..), Container(..),
                       Hugr, -- do NOT export contents, keep abstract
-                      newWithIO, newModule, splitNamespace, rootNode,
+                      newWithIO, newModule, splitNamespace,
                       freshNode, freshNodeWithIO,
                       setOp, getParent, getOp,
                       addEdge, addOrderEdge,
@@ -12,7 +12,7 @@ import Brat.Naming
 
 import Data.Hugr hiding (Hugr)
 import qualified Data.Hugr as D
-import Data.List (partition, sortBy)
+import Data.List (sortBy)
 import Data.Maybe (fromMaybe)
 import Data.Ord (comparing)
 import Data.Tuple (swap)
@@ -27,32 +27,27 @@ data Container = Ctr {
 }
 
 data Hugr = HugrGraph {
-    -- exactly one node (the root) will have parent == self
-    parents :: M.Map NodeId NodeId,
+    root :: NodeId,
+    parents :: M.Map NodeId NodeId, -- definitive list of (valid) nodes, excluding root
     nodes :: M.Map NodeId HugrOp,
     edges_out :: M.Map NodeId [(Int, PortId NodeId)],
     edges_in :: M.Map NodeId [(PortId NodeId, Int)],
     nameSupply :: Namespace
 } deriving (Eq, Show) -- we probably want a better `show`
 
--- Quite inefficient on arbitrary Hugr but used only when we know there are few nodes
-rootNode :: Hugr -> NodeId
-rootNode HugrGraph {parents} = let [root] = [node | (node,parent) <- M.assocs parents, node == parent]
-                           in root
-
 splitNamespace :: Hugr -> String -> (Namespace, Hugr)
 splitNamespace hugr n = let (nsx, nsNew) = split n (nameSupply hugr)
                         in (nsx, hugr {nameSupply = nsNew})
 
 freshNode :: Hugr -> NodeId -> String -> (NodeId, Hugr)
-freshNode hugr@(HugrGraph {parents, nameSupply}) parent nam =
+freshNode hugr@(HugrGraph {root, parents, nameSupply}) parent nam =
   case M.lookup parent parents of
-    Nothing -> error "parent does not exist"
-    Just _ -> let (freshName, newSupply) = fresh nam nameSupply
-              in (NodeId freshName, hugr {
-                nameSupply = newSupply,
-                parents = M.alter (\Nothing -> Just parent) (NodeId freshName) parents
-                })
+    Nothing | parent /= root-> error "parent does not exist"
+    _ -> let (freshName, newSupply) = fresh nam nameSupply
+         in (NodeId freshName, hugr {
+              nameSupply = newSupply,
+              parents = M.alter (\Nothing -> Just parent) (NodeId freshName) parents
+            })
 
 freshNodeWithIO :: Hugr -> NodeId -> String -> (Container, Hugr)
 freshNodeWithIO h gparent desc =
@@ -74,29 +69,31 @@ newWithIO ns nam op =
   let (name, ns1) = fresh nam ns
       (input, ns2) = fresh (nam ++ "_Input") ns1
       (output, ns3) = fresh (nam ++ "_Output") ns2
-      node = NodeId name
+      root = NodeId name
   in (HugrGraph {
-        parents = M.fromList ((, node) <$> [node, NodeId input, NodeId output]),
-        nodes = M.singleton node op,
+        root,
+        parents = M.fromList ((, root) . NodeId <$> [input, output]),
+        nodes = M.singleton root op,
         edges_in = M.empty,
         edges_out = M.empty,
         nameSupply = ns3
       }
-     ,Ctr node (NodeId input) (NodeId output)
+     ,Ctr root (NodeId input) (NodeId output)
      )
 
 newModule :: Namespace -> String -> (Hugr, NodeId)
 newModule ns nam =
   let (name, ns') = fresh nam ns
-      node = NodeId name
+      root = NodeId name
   in (HugrGraph {
-        parents = M.singleton node node,
-        nodes = M.singleton node (OpMod ModuleOp),
+        root,
+        parents = M.empty,
+        nodes = M.singleton root (OpMod ModuleOp),
         edges_in = M.empty,
         edges_out = M.empty,
         nameSupply = ns'
       }
-     , node
+     , root
      )
 
 addEdge :: Hugr -> (PortId NodeId, PortId NodeId) -> Hugr
@@ -125,15 +122,13 @@ getOp :: Hugr -> NodeId -> HugrOp
 getOp HugrGraph {nodes} n = nodes M.! n
 
 serialize :: Hugr -> D.Hugr Int
-serialize hugr@(HugrGraph {nodes, parents}) = D.Hugr (
+serialize hugr@(HugrGraph {root, nodes, parents}) = D.Hugr (
     [(transNode parent, op) | (op, parent) <- snd <$> sortedNodes],
     [(Port (transNode s) o, Port (transNode t) i) | (Port s o, Port t i) <- edgeList hugr]
   ) where
     sortedNodes :: [(NodeId, (HugrOp, NodeId))] -- name, (op, parent)
-    sortedNodes = let nodesWithParents = [(name, (nodes M.! name, parent)) | (name, parent) <- M.assocs parents]
-                      isRoot (name, (_op, parent)) = name == parent
-                      ([root], rest) = partition isRoot nodesWithParents
-                  in root:(sortBy (comparing swap) rest)
+    sortedNodes = let withOp (name, parent) = (name, (nodes M.! name, parent))
+                  in (withOp (root, root)):(sortBy (comparing swap) (withOp <$> M.assocs parents))
 
     transNode :: NodeId -> Int
     transNode = ((M.fromList $ zip (fst <$> sortedNodes) [0..]) M.!)
