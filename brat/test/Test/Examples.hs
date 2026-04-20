55e392982dd167306ac9cd9f6d4df1027b74bac5
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-04-20 15:52:07 +0100

    Change --!test to --!exec w/option -xfail, add tests in app.brat

--- brat/test/Test/Examples.hs
+++ brat/test/Test/Examples.hs
@@ -9,6 +9,7 @@ import Data.Char (isAlphaNum)
 import Data.Functor ((<&>))
 import Data.List (isPrefixOf)
 import qualified Data.Text.Lazy as T
+import Data.Maybe (fromJust)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.Silver
@@ -17,7 +18,7 @@ import Test.Tasty.ExpectedFailure
 --import Debug.Trace
 
 execTestPrefix :: T.Text
-execTestPrefix = T.pack "--!test "
+execTestPrefix = T.pack "--!exec"
 
 interpreterOutputPrefix :: String
 interpreterOutputPrefix = "Finished "
@@ -41,17 +42,22 @@ getExamplesTests =  do
        else
         let interpreterTests = T.breakOnAll execTestPrefix (T.pack cts) <&> \(_, start) ->
               let (testLine, newlineDefn) = T.breakOn (T.pack "\n") start
-                  expectedOutput = interpreterOutputPrefix ++ T.unpack (T.drop (T.length execTestPrefix) testLine)
+                  -- testLine begins with execTestPrefix, then either " " or "-xfail " and the expected result
+                  restLine = fromJust $ T.stripPrefix execTestPrefix testLine
+                  (is_xfail, eOut) = case T.stripPrefix (T.pack "-xfail ") restLine of
+                    Just out -> (True, out)
+                    Nothing -> (False, restLine) -- assume begins with space, will be removed here:
+                  expectedOutput = interpreterOutputPrefix ++ T.unpack (T.strip eOut)
                   -- this repeats/roughly duplicates the logic for "identifiers" in the parser
                   func_name = T.unpack $ T.takeWhile (\c -> isAlphaNum c || c == '_' || c == '\'') (T.drop 1 newlineDefn)
-              in testCase func_name $ do
+              in (if is_xfail then expectFail else id) $ testCase func_name $ do
                   -- this completely recompiles the file for each test, which is pretty bad
                   output <- runInterpreter [] path func_name
-                  expectedOutput @?= (T.unpack output)
+                  (T.unpack output) @?= expectedOutput
             compileTest = compileToOutput "compilation" path
             checkAndCompile = if isPrefixOf "--!xfail-compilation" cts
               then [checkTest, expectFail compileTest] else [compileTest]
         in case interpreterTests of
           [] -> testGroup (show path) checkAndCompile
           intTests -> sequentialTestGroup path AllSucceed
-              (checkAndCompile ++ [testGroup "execution" interpreterTests])
+              (checkAndCompile ++ [testGroup "execution" intTests])
