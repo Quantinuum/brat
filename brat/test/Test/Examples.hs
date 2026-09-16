6d6a33e125c491971feb85f891d9369e15362e2c
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-09-16 15:23:28 +0100

    rowToRo does the necessary hasochism magic

--- brat/test/Test/Examples.hs
+++ brat/test/Test/Examples.hs
@@ -1,23 +1,24 @@
 module Test.Examples (getExamplesTests) where
 
 import Brat.Parser (parseExpr)
-import Brat.Checker (check, checkWithGraph)
-import Brat.Checker.Helpers (anext, rowToRo)
+import Brat.Checker (checkWithGraph)
+import Brat.Checker.Helpers (anext, next, rowToRo)
 import Brat.Checker.Monad (Checking)
-import Brat.Checker.Types (Unders, Overs)
+import Brat.Checker.Types (Unders)
 import Brat.Compiler (compileToGraph)
 import Brat.Elaborator (elaborateChkNoun)
+import Brat.Error (showError)
 import Brat.FC (WC(..))
 import Brat.Graph (NodeType(..))
 import Brat.Load (VMod, checkDecl, parseFile)
 import Brat.Machine (interpretGraph)
 import Brat.Naming (Namespace)
 import Brat.QualName (plain)
-import Brat.Syntax.Common (Mode(..), Dir(..), Kind(..), Modey(..), Tgt)
+import Brat.Syntax.Common (Mode(..), Dir(..), Kind(..), Modey(..))
 import Brat.Syntax.FuncDecl (FuncDecl(..), FunBody(..), Locality(..))
-import Brat.Syntax.Port (NamedPort(..))
+import Brat.Syntax.Port (End)
 import Brat.Syntax.Raw (runDesugar, Desugarable(..), Raw, RawEnv)
-import Brat.Syntax.Value (BinderType, CTy(..), Ro(..), Stack(..), Val(..), VDecl(..))
+import Brat.Syntax.Value (AddR(..), CTy(..), Ro(..), Stack(..), Val(..), VarChanger(..), VDecl(..), stkLen, varChangerThroughRo)
 import Brat.Syntax.Core (Term(..))
 import Test.Checking (parseAndCheckNamed)
 
@@ -25,7 +26,6 @@ import Hasochism (N(..), Ny(..), Some(..), (:*)(..))
 
 import Control.Exception (catch)
 import qualified Data.ByteString as BS
-import Data.Bifunctor (first)
 import Data.Char (isAlphaNum)
 import Data.Functor ((<&>))
 import Data.Hugr (isHole)
@@ -64,48 +64,54 @@ funcTest nsmod path func_name testTy = case testTy of
           Left err -> assertFailure ("Could not parse arguments: " ++ show err)
           Right val -> pure val
         (WC fc raw_arg_noun) :: WC (Raw Chk Noun) <- case elaborateChkNoun arg of
-          Left err -> assertFailure ("Could not elaborate arguments: " ++ show err)
+          Left err -> assertFailure ("Could not elaborate arguments: " ++ showError err)
           Right val -> pure val
         
-        --let io :: TypeRowElem (WC (KindOr RawVType)) = -- VType -> RawVType = RawChkNoun, no thanks
-
-        --let decl :: FuncDecl [RawIO] (FunBody Raw Noun) = (FuncDecl
-        --  io
-        --  NoLhs (WC fc (RForce (WC fc (RVar (plain func_name)))) ::$:: (WC fc raw_arg_noun)))
-        -- let env :: RawEnv = ([decl], [], M.empty)
-        -- ([decl], []) <- case desugarEnv env of ...
         let env :: RawEnv = ([], [], M.empty) -- ALAN will this work? E.g. args referring to other funcs (higher-order)?
         arg_noun <- case runDesugar env (desugar' raw_arg_noun) of
-          Left err -> assertFailure ("Could not desugar arguments: " ++ show err)
+          Left err -> assertFailure ("Could not desugar arguments: " ++ showError err)
           Right val -> pure val
         let body :: FunBody Term Noun = NoLhs $ WC fc $ (WC fc $ Emb $ WC fc $ Force (WC fc (Var (plain func_name)))) :$: (WC fc arg_noun)
         let test_func_name = "test_" ++ func_name -- NO need to make unique
         
         let (ns, (oldDeclEnv, oldHoles, oldStore, oldGraph, oldCaps)) = nsmod
-        oldHoles @?= [] -- do we need to skip interpreting if there are holes?
+        length oldHoles @?= 0 -- do we need to skip interpreting if there are holes?
         let (_, VDecl fd) = oldDeclEnv M.! (plain func_name)
         functy :: CTy Brat Z <- case (fnSig fd) of
             Some (RPr (_, VFun Braty cty) R0) -> pure cty
             io -> assertFailure ("Can only test single functions, not: " ++ show io)
         let doCheck :: Checking () = case functy of
+              -- Should we split the namespace here?
               (ins :->> outs) -> do
                 -- We're gonna check a function application, i.e. `body` above, but we must
                 -- put that inside a VDecl, which requires declaring its types :(.
-                (_, unders :: Unders Brat Chk, overs :: Overs Brat UVerb, _) <- anext "kindchecktest" Hypo (S0, Some (Zy :* S0)) ins outs
+                (_, unders :: Unders Brat Chk, _overs, _) <- anext "kindchecktest" Hypo (S0, Some (Zy :* S0)) ins outs
                 -- the args will be plugged into overs, but the application checks that
-                let underPorts :: [(String, BinderType Brat)] = map (first portName) unders
                 -- TODO do we need a non-empty stack here?
-                outs :: Some (Ro Brat Z) <- rowToRo Braty underPorts S0 <&> \case
-                  Some (ro :* _) -> Some ro
-                let decl = VDecl (FuncDecl test_func_name outs body fc Local)
-                -- Should we split the namespace here?
-                checkDecl [test_func_name] decl outs
+                outs :: Some (Ro Brat Z :* Stack Z End) <- rowToRo Braty unders S0
+
+                let decl_outs = case outs of
+                      Some (ro :* _) -> Some ro
+                    decl = VDecl (FuncDecl test_func_name decl_outs body fc Local)
+
+                -- The decl needs wiring into an Id node whose *inputs* are the outs we just obtained,
+                -- and whose *outputs* are another copy of that, hasochistically renumbered to come after.
+                unders <- case outs of
+                      Some (id_ins :* ends) -> case varChangerThroughRo (ParToInx (AddZ $ stkLen ends) ends) id_ins of
+                        Some (_ :* id_outs) -> do
+                          (_, unders, _, _) <- next test_func_name Id (S0, Some (Zy :* S0)) id_ins id_outs
+                          pure unders
+
+                -- Finally check the decl onto that Id node
+                checkDecl [test_func_name] decl unders
         ((), (noHoles, newStore, newGraph, noCaps)) <- case checkWithGraph (M.map fst oldDeclEnv) oldStore ns oldGraph doCheck of
-          Left err -> assertFailure ("Could not check arguments: " ++ show err)
+          Left err -> assertFailure ("Could not check arguments: " ++ showError err)
           Right val -> pure val
-        (noCaps, noHoles) @?= (M.empty, []) -- i.e. in arguments
-        let nsmod = (ns, (oldDeclEnv, oldHoles, newStore, newGraph, oldCaps))
+        -- sanity check the arguments
+        noCaps @?= M.empty
+        length noHoles @?= 0
         
+        let nsmod = (ns, (oldDeclEnv, oldHoles, newStore, newGraph, oldCaps))
         hugr <- case interpretGraph nsmod test_func_name of
               Left s -> assertFailure $ "Expected hugr, got " ++ T.unpack s
               Right hugr -> pure hugr
